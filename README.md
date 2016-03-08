705f23d030049f85a10a73d7b679a7f6111597de
Danny Zijlstra <d.zijlstra@directdevice.nl>
2016-03-08 12:32:03 +0100

    added extra files

---
+++ README.md
@@ -0,0 +1,14 @@
+#Dotfiles
+
+My personal dotfiles.
+
+
+##Installation
+
+Open a terminal and execute the following commands:
+
+```sh
+git clone https://github.com/juniorzjr/dotfiles.git
+cd dotfiles
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH=$(cd "$(dirname "${0}")"; echo $(pwd))
+
+# create symlink to this repo
+rm -rf ~/.dotfiles
+ln -s $SCRIPTPATH ~/.dotfiles
+
+# setup bash
+rm ~/.bash_profile
+ln -s $SCRIPTPATH/bash_profile ~/.bash_profile
+
+# setup git
+rm ~/.gitconfig
+rm ~/.gitignore
+ln -s $SCRIPTPATH/gitconfig ~/.gitconfig
+ln -s $SCRIPTPATH/gitignore ~/.gitignore
+
+# setup tmux
+rm ~/.tmux.conf
+ln -s $SCRIPTPATH/tmux.conf ~/.tmux.conf
+
